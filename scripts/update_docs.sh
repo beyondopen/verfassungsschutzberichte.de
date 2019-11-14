b6ad4f06e805249831689092b244b852159161ac
Johannes Filter <hi@jfilter.de>
2019-11-14 16:07:56 +0100

    add script to update docs

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -1,3 +1,4 @@
-# docker exec -it vsb bash -c "flask init-docs"
+#!/usr/bin/env bash
+set -x
 
-docker exec -it vsb bash -c "flask update-docs '*'"
+ssh app -t "dokku run vsb flask update-docs '*' && dokku run vsb flask clear-cache && cd /home/filter/code/verfassungsschutzberichte.de/scripts && bash prefill_cache.sh && sudo bash create_zips.sh"
