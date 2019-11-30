e8060b62af4b73724c450ad6f2b3f566705945a4
Johannes Filter <hi@jfilter.de>
2019-12-01 00:09:17 +0100

    update

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -3,3 +3,5 @@ set -e
 set -x
 
 convert -compress Zip -density 150x150 $1 compressed.$1
+
+# gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf ~/Desktop/vs.pdf
