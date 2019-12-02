c957c24e0b90890b3b3b5c7ed5f0979c516523bc
Johannes Filter <hi@jfilter.de>
2019-12-02 22:54:34 +0100

    improve reports page

--- src/report_info.py
+++ src/report_info.py
@@ -1,5 +1,48 @@
-report_info = {
+title = {
     "Sachsen-Anhalt": {1992: {"title": "1992/1993"}},
     "Thüringen": {2015: {"title": "2014/2015"}},
+    "Niedersachsen": {1984: {"title": "1983/1984"}},
 }
+
+start_year = {
+    "Sachsen-Anhalt": 1992,
+    "Thüringen": 1991,
+    "Niedersachsen": 1984,
+    "Hamburg": 1993,
+    "Bremen": 2002,
+    "Berlin": 1990,
+    "Bund": 1968,
+    "Baden-Württemberg": 1975,
+    "Bayern": 1976,
+    "Brandenburg": 1993,
+    "Hessen": 1977,
+    "Mecklenburg-Vorpommern": 1992,
+    "Saarland": 2013,
+    "Schleswig-Holstein": 1976,
+    "Nordrein-Westfalen": 1950,
+    "Sachsen": 1993,
+    "Rheinland-Pfalz": 1984,
+}
+
+no_reports = {
+    "Sachsen-Anhalt": [],
+    "Thüringen": [2014],
+    "Niedersachsen": [],
+    "Hamburg": [],
+    "Bremen": [],
+    "Berlin": [],
+    "Bund": [],
+    "Baden-Württemberg": [],
+    "Bayern": [],
+    "Brandenburg": [],
+    "Hessen": [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    "Mecklenburg-Vorpommern": [],
+    "Saarland": [],
+    "Schleswig-Holstein": [],
+    "Nordrein-Westfalen": [],
+    "Sachsen": [],
+    "Rheinland-Pfalz": [],
+}
+
+report_info = {"title": title, "start_year": start_year, "no_reports": no_reports}
 
