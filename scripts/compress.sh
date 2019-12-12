43031558a563a93bf436b999cb678eb6d27e22c9
Johannes Filter <hi@jfilter.de>
2019-12-12 15:34:07 +0100

    add better script for compressing

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -2,6 +2,10 @@
 set -e
 set -x
 
-convert -compress Zip -density 150x150 $1 compressed.$1
+# convert -compress Zip -density 150x150 $1 compressed.$1
 
 # gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf ~/Desktop/vs.pdf
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
+    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
+    -dCompressFonts=true -r150 -sOutputFile=$1.output.pdf $1
