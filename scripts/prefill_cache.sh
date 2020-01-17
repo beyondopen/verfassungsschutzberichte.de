55c0a4b83dca675625490f72b88579aac81e9e18
Johannes Filter <hi@jfilter.de>
2020-01-17 16:56:53 +0100

    use http for backend

--- scripts/prefill_cache.sh
+++ scripts/prefill_cache.sh
@@ -3,4 +3,4 @@ set -e
 set -x
 
 # download but through away
-wget -e robots=off --delete-after --verbose --recursive --level 2 https://vsb.app.vis.one 
+wget -e robots=off --delete-after --verbose --recursive --level 2 http://vsb.app.vis.one 
