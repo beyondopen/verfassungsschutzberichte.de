819ec1917592b7cd8ed42c7db7bb0e8b152b2c78
Johannes Filter <hi@jfilter.de>
2020-06-06 00:41:38 +0200

    clean up scripts

--- scripts/create_zips.sh
+++ scripts/create_zips.sh
@@ -6,9 +6,9 @@ cd /mnt/data/temp-data && /usr/bin/zip -r -j /mnt/data/temp-data/vsberichte.zip 
 
 mkdir -p /mnt/data/temp-data/tmp-texts/ &&
 rm /mnt/data/temp-data/tmp-texts/* ;
-curl https://vsb.app.vis.one/api/ | jq -r '.reports[].jurisdiction_escaped' | while read -r line; do
+curl http://vsb.app.vis.one/api | jq -r '.reports[].jurisdiction_escaped' | while read -r line; do
   for y in {1950..2020}; do
-    wget https://vsb.app.vis.one/$line-$y.txt -P /mnt/data/temp-data/tmp-texts
+    wget http://vsb.app.vis.one/$line-$y.txt -P /mnt/data/temp-data/tmp-texts
   done
 done
 
--- scripts/wrangle_pdf/reverse.sh
+++ scripts/wrangle_pdf/reverse.sh
@@ -2,4 +2,6 @@
 set -e
 set -x
 
+# qpd: https://askubuntu.com/a/1127083
+
 pdftk $1 cat end-1 output $1.reversed
