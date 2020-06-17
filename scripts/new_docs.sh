5c99219257737ba677012b12e7c72aa402d68508
Johannes Filter <hi@jfilter.de>
2020-06-17 14:39:47 +0200

    also upload raw files

--- scripts/new_docs.sh
+++ scripts/new_docs.sh
@@ -4,6 +4,8 @@ set -x
 # $1: location of folder with PDF-files
 # $2: `process` or `upload`
 
+# TODO: right now, you have to manually create a .raw folder to upload raw files
+
 if [[ "$1" != /* ]]; then
     echo "only absolute paths"
     exit 1
@@ -17,8 +19,9 @@ fi
 
 if [ "$2" == "upload" ]; then
     echo 'uploading to server'
-    scp "$1"/* md:/mnt/data/vsb/vsb-all/raw/
+    scp "$1"/* md:/mnt/data/vsb/vsb-all/cleaned/
     scp "$1".done/* md:/mnt/data/vsb/vsb-data/pdfs/
+    [ -d "$1".raw ] && scp "$1".raw/* md:/mnt/data/vsb/vsb-all/raw/
     ./update_docs.sh
     exit 0
 fi
