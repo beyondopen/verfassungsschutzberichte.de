6593827153c23bbeef9326723e40d845fc235808
Johannes Filter <hi@jfilter.de>
2020-09-26 17:38:42 +0200

    don't prefetch

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-ssh md -t "dokku run vsb flask update-docs '*' && dokku run vsb flask clear-cache && cd /home/filter/code/verfassungsschutzberichte.de/scripts && bash prefill_cache.sh && sudo bash create_zips.sh"
+ssh md -t "dokku run vsb flask update-docs '*' && dokku run vsb flask clear-cache && cd /home/filter/code/verfassungsschutzberichte.de/scripts && sudo bash create_zips.sh"
