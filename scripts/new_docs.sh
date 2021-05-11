0fbd11b7d7b314c59eca6bb5af18ef86561ae209
Johannes Filter <hi@jfilter.de>
2021-05-11 20:06:16 +0200

    add safeguard to new doc script

--- scripts/new_docs.sh
+++ scripts/new_docs.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 set -x
 
-# Arguments: 
+# Arguments:
 # $1: location of a folder with PDF files
 # $2: `process` or `upload`
 
 # Usage:
-# ./new_docs thefolder process 
+# ./new_docs thefolder process
 # ./new_docs thefolder upload
 
 # Right now, you have to manually create a `thefolder.raw` folder to upload raw files.
 
 if [[ "$1" != /* ]]; then
     echo "only absolute paths"
+    exit 1
+fi
+
+path=$1
+if [[ "${path: -1}" == "/" ]]; then
+    echo "remove trailing slash"
     exit 1
 fi
 
